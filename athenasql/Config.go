6bf33c68eb216dbae602e7284fce32b6f01398a8
Vlastimil Šenfeld <bobesa@gammosaur.com>
2018-12-05 14:42:25 +0100

    ++config

---
+++ athenasql/Config.go
@@ -0,0 +1,42 @@
+package athenasql
+
+import (
+	"fmt"
+	"strings"
+)
+
+// Config contains all configuration from passed dsn
+type Config struct {
+	Region           string
+	SessionGenerator string
+	S3Bucket         string
+}
+
+// ConfigFromDSN returns new config based on given dsn
+func ConfigFromDSN(dsn string) (Config, error) {
+	dsn = strings.TrimSpace(dsn)
+	if dsn == "" {
+		return Config{}, nil
+	}
+
+	// TODO: Make this somewhat better, this is just temporary
+	cfg := Config{}
+	for _, part := range strings.Split(dsn, " ") {
+		pair := strings.SplitN(part, "=", 2)
+		if len(pair) != 2 {
+			return Config{}, fmt.Errorf(`%q is not a key-value pair (foo=bar)`, part)
+		}
+
+		switch strings.ToLower(pair[0]) {
+		case "session_generator":
+			cfg.SessionGenerator = pair[1]
+		case "s3_bucket":
+			cfg.S3Bucket = pair[1]
+		case "region":
+			cfg.Region = pair[1]
+		default:
+			return Config{}, fmt.Errorf(`%q is not a valid parameter`, pair[0])
+		}
+	}
+	return cfg, nil
+}
---
+++ athenasql/Config_test.go
@@ -0,0 +1,49 @@
+package athenasql
+
+import "testing"
+
+func TestConfigFromDSN(t *testing.T) {
+	for _, testCase := range []struct {
+		DSN    string
+		Config Config
+		Errors bool
+	}{
+		// Empty SDN == Empty config
+		{
+			Config: Config{},
+		},
+
+		// Read Region
+		{
+			DSN:    "region=aaa",
+			Config: Config{Region: "aaa"},
+		},
+
+		// Read S3 Bucket
+		{
+			DSN:    "s3_bucket=aaa",
+			Config: Config{S3Bucket: "aaa"},
+		},
+
+		// Read Session Generator
+		{
+			DSN:    "session_generator=aaa",
+			Config: Config{SessionGenerator: "aaa"},
+		},
+
+		// Read All of 3 above
+		{
+			DSN:    "session_generator=sg region=rrr s3_bucket=s3b",
+			Config: Config{SessionGenerator: "sg", Region: "rrr", S3Bucket: "s3b"},
+		},
+	} {
+		cfg, err := ConfigFromDSN(testCase.DSN)
+		if err == nil && testCase.Errors {
+			t.Errorf(`Expected %q to error, but did not`, testCase.DSN)
+		} else if err != nil && !testCase.Errors {
+			t.Errorf(`Expected not errors for %q, but got %q instead`, testCase.DSN, err.Error())
+		} else if cfg != testCase.Config {
+			t.Errorf(`Expected %v for %q, but got %v instead`, testCase.Config, testCase.DSN, cfg)
+		}
+	}
+}
